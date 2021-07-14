b060b2f4ae3258f534bbbe3851bfe620a1fa7fe0
Le Xuan Quang <quang.lxun@gmail.com>
2021-07-14 16:20:11 +0700

    add relationships

---
+++ app/Models/Document.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
+use App\Models\Category;
+
+class Document extends Model
+{
+    public function downloads()
+    {
+        return $this->belongsToMany(User::class, 'downloads')->withTimestamps();
+    }
+
+    public function favorites()
+    {
+        return $this->belongsToMany(User::class, 'favorites')->withTimestamps();
+    }
+
+    public function comments()
+    {
+        return $this->belongsToMany(User::class, 'comments')->withPivot('content')->withTimestamps();
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+}
